6c011f8dafae31e80d660d95ed3e0af42a98386a
Olivier DALET <odalet@gmail.com>
2017-02-05 14:17:31 +0100

    Fixing unit-tests.ps1

--- unit-tests.ps1
+++ unit-tests.ps1
@@ -2,7 +2,7 @@ C:\projects\delta-units\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe  
 -register:Path64 `
 "-target:C:\projects\delta-units\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" `
 "-targetargs:UnitTests\UnitTests.Delta.Units\bin\$env:CONFIGURATION\UnitTests.Delta.Units.dll UnitTests\UnitTests.Delta.Units.Globalization\bin\$env:CONFIGURATION\UnitTests.Delta.Units.Globalization.dll -appveyor -noshadow" `
-"-filter:-[UnitTests*]*" `
+"-filter:+[Delta.Units*]* -[UnitTests*]*" `
 "-output:opencoverCoverage.xml"
 
 $coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
