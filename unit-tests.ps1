1f57f1603360b0c0f0bb0b52d7e4d94fab20cc60
Olivier DALET <odalet@gmail.com>
2017-02-05 13:58:28 +0100

    Fixing opencover filter

--- unit-tests.ps1
+++ unit-tests.ps1
@@ -2,7 +2,7 @@
 -register:user `
 -target:"C:\projects\$env:APPVEYOR_PROJECT_NAME\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" `
 "-targetargs:UnitTests\UnitTests.Delta.Units\bin\$env:CONFIGURATION\UnitTests.Delta.Units.dll UnitTests\UnitTests.Delta.Units.Globalization\bin\$env:CONFIGURATION\UnitTests.Delta.Units.Globalization.dll -appveyor -noshadow" `
--filter "-[*Test]*" `
+-filter: "-[UnitTests*]*" `
 -output:opencoverCoverage.xml
 $coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
 
