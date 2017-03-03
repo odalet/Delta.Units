14466685e28ae782fbeee8952f3dc13425daf492
Olivier DALET <odalet@gmail.com>
2017-03-03 16:56:18 +0100

    Bumped version to 0.2

--- Delta.Units/Properties/AssemblyInfo.cs
+++ Delta.Units/Properties/AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
 
 [assembly: InternalsVisibleTo("UnitTests.Delta.Units")]
 [assembly: InternalsVisibleTo("UnitTests.Delta.Units.Globalization")]
