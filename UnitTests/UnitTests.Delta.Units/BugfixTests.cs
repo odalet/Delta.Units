e62a4855bec8a8da4e6fdc9bfce2da61bfc7016c
Olivier DALET <odalet@gmail.com>
2018-05-08 16:47:27 +0200

    Bug repro: 1 Are should be 100 m², not 10000 m²

---
+++ UnitTests/UnitTests.Delta.Units/BugfixTests.cs
@@ -0,0 +1,19 @@
+﻿using Delta.Units;
+using Xunit;
+
+namespace Delta.Units
+{
+    public class BugfixTests
+    {
+        [Fact]
+        public void One_are_is_100_square_metres()
+        {
+            var m = new Unit("m", "m", BaseDimensions.Length);
+            var a = new Unit("a", "a", m * m, 100m);
+
+            var oneAre = 1 * a;
+            var sqm = oneAre.ConvertTo(m * m);
+            Assert.Equal(100m, sqm.Value);
+        }
+    }
+}
